71250fbf66fced2401c7dd7cb0ecc0b1555d05a6
jing <lijing_ziqing@163.com>
2019-08-02 11:13:56 +0800

    123

--- src/main/java/com/wulouzi/operators/TestProject.java
+++ src/main/java/com/wulouzi/operators/TestProject.java
@@ -9,18 +9,18 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class TestProject {
     private static final Tuple4<String, Integer, Long, Long>[] ORDERS = new Tuple4[]{
-            Tuple4.of("1",123, System.currentTimeMillis(),100),
-            Tuple4.of("2",123, System.currentTimeMillis(),200),
-            Tuple4.of("3",123, System.currentTimeMillis(),300),
-            Tuple4.of("4",123, System.currentTimeMillis(),400)
+            Tuple4.of("1", 123, System.currentTimeMillis(), 100),
+            Tuple4.of("2", 123, System.currentTimeMillis(), 200),
+            Tuple4.of("3", 123, System.currentTimeMillis(), 300),
+            Tuple4.of("4", 123, System.currentTimeMillis(), 400)
     };
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<Tuple4<String,Integer,Long,Long>> input = env.fromElements(ORDERS);
+        DataStream<Tuple4<String, Integer, Long, Long>> input = env.fromElements(ORDERS);
 
-        DataStream<Tuple3<String,Long,Long>> projectStream = input.project(0,2,3);
+        DataStream<Tuple3<String, Long, Long>> projectStream = input.project(0, 2, 3);
 
         projectStream.print();
 
